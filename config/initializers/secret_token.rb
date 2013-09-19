088db6828d09680d7c191e294ff3713abbd5e509
Benoit Lafontaine <joel1di1@gmail.com>
2013-09-19 22:07:06 +0200

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Martigua::Application.config.secret_key_base = '1fa74a825daa47933260a273144d3a51b6a8c4668d90a86a39591b098d505192a1b6652b2e6b512dcda46c48973e9647e0cc9ab78ee5c175d0b77cc06b95c9ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Martigua::Application.config.session_store :cookie_store, key: '_martigua_session'
