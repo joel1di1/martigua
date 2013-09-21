b17dbe8ad4e4a4b0068d118b36bdfa045662431d
Benoit Lafontaine <joel1di1@gmail.com>
2013-09-21 15:56:57 +0200

    add nickname and phone on users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919214254) do
+ActiveRecord::Schema.define(version: 20130921080244) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20130919214254) do
     t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
+    t.string   "phone"
   end
 
 end
