eeb81369aca1faddc85d3f2a29451c608de34c34
Benoit Lafontaine <joel1di1@gmail.com>
2013-10-04 00:16:16 +0200

    clean fliters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
 
-  before_filter :authenticate_user!
-
   def index
     @club = Club.find_by_name 'Martigua' 
 
--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,5 +1,5 @@
 class PingController < ApplicationController
-  skip_before_filter :authenticate_user!
+  skip_before_filter :authenticate_user_or_token!
 
   def index
     render json: {time: Time.now}
