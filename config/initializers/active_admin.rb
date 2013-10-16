14f45b29ae7c33d1fb249f8bca16b870f6c5b99d
Benoit Lafontaine <joel1di1@gmail.com>
2013-10-16 10:33:51 +0200

    une seule connexion pour l partie admin merci

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -200,7 +200,7 @@ ActiveAdmin.setup do |config|
   # Pagination is enabled by default for all resources.
   # You can control the default per page count for all resources here.
   #
-  # config.default_per_page = 30
+  config.default_per_page = 50
 
 
   # == Filters
@@ -210,5 +210,7 @@ ActiveAdmin.setup do |config|
   # You can enable or disable them for all resources here.
   #
   # config.filters = true
+
+  config.skip_before_filter :authenticate_user_or_token!
 
 end
