c720f96022ad2820c220af57bcca058c2f536ff2
Benoit Lafontaine <joel1di1@gmail.com>
2013-10-16 12:23:13 +0200

    add sort by starting date or between_day1

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,4 +5,8 @@ class MatchesController < InheritedResources::Base
       params[:team_id] ? Team.find(params[:team_id]) : nil
     end
 
+    def collection
+      @matches ||= end_of_association_chain.order('LEAST(starting_time, between_day1)')
+    end
+
 end
