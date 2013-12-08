31f6aa16c442e938b7c37fa4845827abd546a8e4
Benoit Lafontaine <joel1di1@gmail.com>
2013-12-08 15:30:11 +0100

    format

--- app/models/match_day.rb
+++ app/models/match_day.rb
@@ -1,9 +1,9 @@
 class MatchDay < ActiveRecord::Base
   has_many :matches
 
-  scope :next,     -> { where('end_date >= ?', Date.today).order('end_date ASC').first }
-  scope :previous, -> { where('start_date <= ?', Date.today).order('start_date DESC').first }
+  scope :next,      -> { where('end_date >= ?', Date.today).order('end_date ASC').first }
   scope :next_in_7, -> { where('end_date >= ?', 2.days.from_now).order('end_date ASC').first }
+  scope :previous,  -> { where('start_date <= ?', Date.today).order('start_date DESC').first }
 
   def futur?
     end_date > Date.today
