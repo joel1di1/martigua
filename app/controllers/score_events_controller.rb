044faa243de7680d36f99d7ff075fe18da48b3ba
Benoit Lafontaine <joel1di1@gmail.com>
2014-02-17 22:12:34 +0100

    score_events only for coachs

--- app/controllers/score_events_controller.rb
+++ app/controllers/score_events_controller.rb
@@ -1,5 +1,7 @@
 class ScoreEventsController < InheritedResources::Base
 
+  before_filter :only_coachs
+
   def permitted_params
     params.permit(:score_event => [:user_id, :label, :value])
   end
