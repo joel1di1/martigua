443669b86b507c0ddf2f5dba9770aff787a1a305
Benoit Lafontaine <joel1di1@gmail.com>
2014-03-12 20:46:09 +0100

    show current training to validate presences

--- app/controllers/coach_home_controller.rb
+++ app/controllers/coach_home_controller.rb
@@ -7,7 +7,7 @@ class CoachHomeController < ApplicationController
     @teams = @club.teams.order('name ASC')
 
     @next_trainings = @club.next_trainings
-    @previous_trainings = @club.previous_trainings
+    @previous_trainings = Training.where('date < ?', 1.day.from_now).order('date DESC').limit(5)
     @users_without_cotis = User.active.without_cotis
     @users_without_certif = User.active.without_certif
   end
